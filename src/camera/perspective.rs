b23627c2707d88ea5c92c68b3d2b3f0a5eef15af
Julius Oeftiger <>
2021-12-15 14:53:34 +0100

    cameras: only have perspective camera

--- src/camera/perspective.rs
+++ src/camera/perspective.rs
@@ -5,102 +5,10 @@ use crate::{Float, Mat4, UVec2, Vec2, Vec3};
 use cgmath::{ElementWise, EuclideanSpace, InnerSpace, Point3, Transform, Zero};
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
-struct CameraConfig {
-    sampler: CameraSampler,
-    eye: Vec3,
-    target: Vec3,
-    up: Vec3,
-    fov: Float,
-    resolution: UVec2,
-}
-
 #[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(from = "PerspectiveConfig")]
-#[serde(into = "PerspectiveConfig")]
+#[serde(from = "CameraConfig")]
+#[serde(into = "CameraConfig")]
 pub struct PerspectiveCamera {
-    sampler: CameraSampler,
-    look_at: Mat4,
-    bot_left: Vec2,
-    top_right: Vec2,
-    res: UVec2,
-    inv_res: Vec2,
-}
-
-impl PerspectiveCamera {
-    pub fn new(
-        sampler: CameraSampler,
-        eye: Vec3,
-        target: Vec3,
-        up: Vec3,
-        fov: Float,
-        resolution: UVec2,
-    ) -> Self {
-        let res = resolution.cast().unwrap();
-        let look_at = Mat4::look_at_lh(Point3::from_vec(eye), Point3::from_vec(target), up);
-        let inv_res = Vec2::new(1.0, 1.0).div_element_wise(res);
-
-        let y = (0.5 * fov).to_radians();
-        let top_right = Vec2::new(y * res.x * inv_res.y, y);
-        let bottom_left = -top_right;
-
-        Self {
-            sampler,
-            look_at,
-            bot_left: bottom_left,
-            top_right,
-            res: resolution,
-            inv_res,
-        }
-    }
-}
-
-#[typetag::serde]
-impl Camera for PerspectiveCamera {
-    fn resolution(&self) -> UVec2 {
-        self.res
-    }
-
-    fn primary_ray(&self, pixel: UVec2) -> Ray {
-        let sample = self.sampler.sample();
-        let direction = self.bot_left
-            + (self.top_right - self.bot_left)
-                .mul_element_wise(pixel.cast().unwrap() + sample)
-                .mul_element_wise(self.inv_res);
-        let direction = direction.extend(1.0).normalize();
-
-        let origin = self.look_at.transform_point(Point3::from_vec(Vec3::zero()));
-        let direction = self.look_at.transform_vector(direction);
-
-        Ray::new(origin.to_vec(), direction)
-    }
-}
-
-#[derive(Deserialize, Serialize)]
-enum PerspectiveConfig {
-    Full(PerspectiveCamera),
-    Config(CameraConfig),
-}
-impl From<PerspectiveConfig> for PerspectiveCamera {
-    fn from(conf: PerspectiveConfig) -> Self {
-        match conf {
-            PerspectiveConfig::Full(c) => c,
-            PerspectiveConfig::Config(c) => {
-                Self::new(c.sampler, c.eye, c.target, c.up, c.fov, c.resolution)
-            }
-        }
-    }
-}
-impl From<PerspectiveCamera> for PerspectiveConfig {
-    fn from(cam: PerspectiveCamera) -> Self {
-        Self::Full(cam)
-    }
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-#[serde(from = "NaiveConfig")]
-#[serde(into = "NaiveConfig")]
-pub struct NaiveCamera {
     sampler: CameraSampler,
     eye: Vec3,
     x_dir: Vec3,
@@ -109,7 +17,7 @@ pub struct NaiveCamera {
     res: UVec2,
 }
 
-impl NaiveCamera {
+impl PerspectiveCamera {
     pub fn new(
         sampler: CameraSampler,
         eye: Vec3,
@@ -138,6 +46,7 @@ impl NaiveCamera {
         let lower_left = target - 0.5 * w * x_dir - 0.5 * h * y_dir;
 
         Self {
+            config: Box::new(config),
             sampler,
             eye,
             x_dir,
@@ -149,7 +58,7 @@ impl NaiveCamera {
 }
 
 #[typetag::serde]
-impl Camera for NaiveCamera {
+impl Camera for PerspectiveCamera {
     fn resolution(&self) -> UVec2 {
         self.res
     }
@@ -165,49 +74,4 @@ impl Camera for NaiveCamera {
 
         Ray::new(self.eye, direction)
     }
-}
-
-#[derive(Deserialize, Serialize)]
-enum NaiveConfig {
-    Full(Naive),
-    Config(CameraConfig),
-}
-impl From<NaiveConfig> for NaiveCamera {
-    fn from(conf: NaiveConfig) -> Self {
-        match conf {
-            NaiveConfig::Full(n) => Self {
-                sampler: n.sampler,
-                eye: n.eye,
-                x_dir: n.x_dir,
-                y_dir: n.y_dir,
-                lower_left: n.lower_left,
-                res: n.res,
-            },
-            NaiveConfig::Config(c) => {
-                Self::new(c.sampler, c.eye, c.target, c.up, c.fov, c.resolution)
-            }
-        }
-    }
-}
-impl From<NaiveCamera> for NaiveConfig {
-    fn from(c: NaiveCamera) -> Self {
-        Self::Full(Naive {
-            sampler: c.sampler,
-            eye: c.eye,
-            x_dir: c.x_dir,
-            y_dir: c.y_dir,
-            lower_left: c.lower_left,
-            res: c.res,
-        })
-    }
-}
-
-#[derive(Deserialize, Serialize)]
-struct Naive {
-    sampler: CameraSampler,
-    eye: Vec3,
-    x_dir: Vec3,
-    y_dir: Vec3,
-    lower_left: Vec3,
-    res: UVec2,
 }
