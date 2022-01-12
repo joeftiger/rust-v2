65f4e6b862e66faa1570f0c38fd814aba4a37ad8
Julius Oeftiger <>
2022-01-12 18:13:37 +0100

    inline operations

--- src/util/packet_impl.rs
+++ src/util/packet_impl.rs
@@ -29,83 +29,98 @@ pub trait PacketOps<T> {
 
 #[allow(clippy::needless_range_loop)]
 impl PacketOps<Float> for [Float; PACKET_SIZE] {
+    #[inline]
     fn add(mut self, rhs: Self) -> Self {
         self.add_assign(rhs);
         self
     }
 
+    #[inline]
     fn add_assign(&mut self, rhs: Self) {
         for i in 0..PACKET_SIZE {
             self[i] += rhs[i];
         }
     }
 
+    #[inline]
     fn sub(mut self, rhs: Self) -> Self {
         self.sub_assign(rhs);
         self
     }
 
+    #[inline]
     fn sub_assign(&mut self, rhs: Self) {
         for i in 0..PACKET_SIZE {
             self[i] -= rhs[i];
         }
     }
 
+    #[inline]
     fn mul(mut self, rhs: Self) -> Self {
         self.mul_assign(rhs);
         self
     }
 
+    #[inline]
     fn mul_assign(&mut self, rhs: Self) {
         for i in 0..PACKET_SIZE {
             self[i] *= rhs[i];
         }
     }
 
+    #[inline]
     fn mul_t(mut self, rhs: Float) -> Self {
         self.mul_assign_t(rhs);
         self
     }
 
+    #[inline]
     fn mul_assign_t(&mut self, rhs: Float) {
         for i in 0..PACKET_SIZE {
             self[i] *= rhs;
         }
     }
 
+    #[inline]
     fn div(mut self, rhs: Self) -> Self {
         self.div_assign(rhs);
         self
     }
 
+    #[inline]
     fn div_assign(&mut self, rhs: Self) {
         for i in 0..PACKET_SIZE {
             self[i] /= rhs[i];
         }
     }
 
+    #[inline]
     fn div_t(mut self, rhs: Float) -> Self {
         self.div_assign_t(rhs);
         self
     }
 
+    #[inline]
     fn div_assign_t(&mut self, rhs: Float) {
         for i in 0..PACKET_SIZE {
             self[i] /= rhs;
         }
     }
 
+    #[inline]
     fn neg(mut self) -> Self {
         self.neg_assign();
         self
     }
 
+    #[inline]
     fn neg_assign(&mut self) {
         for i in 0..PACKET_SIZE {
             self[i] = -self[i];
         }
     }
 
+    #[inline]
     fn is_black(&self) -> bool {
         self.iter().all(|&f| f == 0.0)
     }
